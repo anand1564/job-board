a56deef361d7624d20863b6a9074cde31b349c88
anand1564 <anand8584pandey@gmail.com>
2024-12-04 00:40:20 +0530

    Added schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import http from "http";
-
+import express from "express";
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello, TypeScript!");
 });
-
+const app=express();
+app.get('/',(req,res)=>{
+  res.send('Hello, TypeScript!');
+})
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
